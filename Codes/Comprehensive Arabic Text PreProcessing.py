ac4c15cafabe2e7e6cde9b5e4348acc13fd8c2bd
Fatemeh.V.Unesi <90507840+F-V-Younesi@users.noreply.github.com>
2024-08-13 05:31:50 -0700

    add new emoji detection function to Comprehensive Arabic Text PreProcessing.py

--- Codes/Comprehensive Arabic Text PreProcessing.py
+++ Codes/Comprehensive Arabic Text PreProcessing.py
@@ -15,6 +15,37 @@ import string
 !pip install emoji==1.7
 import emoji
 
+
+def is_emoji(s):
+  emojis=''
+  for c in s:
+    if c in emoji.UNICODE_EMOJI['en'] and c not in emojis:
+      emojis=emojis+c
+  # return ''.join(c for c in s if c in emoji.UNICODE_EMOJI['en'] and c )
+  return emojis
+
+def remove_emojis(data):
+    emoj = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642"
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+                      "]+", re.UNICODE)
+    return re.sub(emoj, '', data)
+  
 def is_emoji(s):
   emojis=''
   for c in s:
@@ -130,7 +161,7 @@ def remove_unicode(text):
     return text
 
 def normalizing(text):
-  without_emoji,emoji_list=remove_emoji(text)
+  without_emoji=remove_emojis(text)
   without_id=remove_id(without_emoji)
   without_hashtag=remove_hashtags(without_id)
   without_html_tags=remove_html_tags(without_hashtag)
